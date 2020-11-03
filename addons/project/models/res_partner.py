f2e194d32a5f55c028df14d3b295dc3b26886d9a
Victor Feyens <vfe@odoo.com>
2020-11-03 11:18:51 +0100

    [POC] project: remove useless field ?
    This field is seemingly unused.
    
    When a m2o field has a linked o2m, it will be computed directly during 
    create method call and won't be computed at the final flush, benefitting 
    from the orm lazyness.

--- addons/project/models/res_partner.py
+++ addons/project/models/res_partner.py
@@ -9,7 +9,6 @@ class ResPartner(models.Model):
     """ Inherits partner and adds Tasks information in the partner form """
     _inherit = 'res.partner'
 
-    task_ids = fields.One2many('project.task', 'partner_id', string='Tasks')
     task_count = fields.Integer(compute='_compute_task_count', string='# Tasks')
 
     def _compute_task_count(self):
