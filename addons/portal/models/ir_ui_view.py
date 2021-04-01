7e7d4817cafaee9728f5c0c9e22988896d68d672
Gorash <chm@odoo.com>
2021-04-01 17:26:18 +0200

    t-cache + update controllers + lazy

--- addons/portal/models/ir_ui_view.py
+++ addons/portal/models/ir_ui_view.py
@@ -20,11 +20,10 @@ class View(models.Model):
         qcontext = super(View, self)._prepare_qcontext()
         if request and getattr(request, 'is_frontend', False):
             Lang = request.env['res.lang']
-            portal_lang_code = request.env['ir.http']._get_frontend_langs()
             qcontext.update(dict(
                 self._context.copy(),
-                languages=[lang for lang in Lang.get_available() if lang[0] in portal_lang_code],
                 url_for=url_for,
                 is_html_empty=is_html_empty,
             ))
+            qcontext['languages'] = [lang for lang in Lang.get_available() if lang[0] in request.env['ir.http']._get_frontend_langs()]
         return qcontext
