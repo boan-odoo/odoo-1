825222c79f05430a1f54d1548be81b4ca2baaf15
Kevin Baptiste <kba@odoo.com>
2021-11-16 10:35:22 +0100

    [IMP] lunch: add 'Sent' status and allow ordering in the future
    Added a new 'Sent' status, meaning the orders have been made to the
    supplier. Once an order is 'Sent' it cannot be modified anymore.
    
    It's no longer possible to order a product after the automatic email has
    been sent, if the supplier is available the next day, the customer can
    add it to their basket for that day instead.
    
    TaskID: 2678064

--- addons/lunch/models/lunch_order.py
+++ addons/lunch/models/lunch_order.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
 
@@ -18,11 +19,12 @@ class LunchOrder(models.Model):
     product_id = fields.Many2one('lunch.product', string="Product", required=True)
     category_id = fields.Many2one(
         string='Product Category', related='product_id.category_id', store=True)
-    date = fields.Date('Order Date', required=True, readonly=True,
-                       states={'new': [('readonly', False)]},
-                       default=fields.Date.context_today)
+    date = fields.Date('Order Date', required=True, store=True, readonly=False, compute='_compute_date')
     supplier_id = fields.Many2one(
         string='Vendor', related='product_id.supplier_id', store=True, index=True)
+    available_today = fields.Boolean(related='supplier_id.available_today')
+    available_nextday = fields.Boolean(related='supplier_id.available_nextday')
+    order_deadline_passed = fields.Boolean(related='supplier_id.order_deadline_passed')
     user_id = fields.Many2one('res.users', 'User', readonly=True,
                               states={'new': [('readonly', False)]},
                               default=lambda self: self.env.uid)
@@ -31,8 +33,9 @@ class LunchOrder(models.Model):
     price = fields.Monetary('Total Price', compute='_compute_total_price', readonly=True, store=True)
     active = fields.Boolean('Active', default=True)
     state = fields.Selection([('new', 'To Order'),
-                              ('ordered', 'Ordered'),
-                              ('confirmed', 'Received'),
+                              ('ordered', 'Ordered'),       # "Internally" ordered
+                              ('sent', 'Sent'),             # Order sent to the supplier
+                              ('confirmed', 'Received'),    # Order received
                               ('cancelled', 'Cancelled')],
                              'Status', readonly=True, index=True, default='new')
     notified = fields.Boolean(default=False)
@@ -75,7 +78,7 @@ class LunchOrder(models.Model):
     def _compute_display_reorder_button(self):
         show_button = self.env.context.get('show_reorder_button')
         for order in self:
-            order.display_reorder_button = show_button and order.state == 'confirmed'
+            order.display_reorder_button = show_button and order.state == 'confirmed' and (order.supplier_id.available_today or order.supplier_id.available_nextday)
 
     def init(self):
         self._cr.execute("""CREATE INDEX IF NOT EXISTS lunch_order_user_product_date ON %s (user_id, product_id, date)"""
@@ -124,7 +127,7 @@ class LunchOrder(models.Model):
                 **vals,
                 'toppings': self._extract_toppings(vals),
             })
-            if lines:
+            if lines.filtered(lambda l: l.state not in ['sent', 'confirmed']):
                 # YTI FIXME This will update multiple lines in the case there are multiple
                 # matching lines which should not happen through the interface
                 lines.update_quantity(1)
@@ -162,10 +165,14 @@ class LunchOrder(models.Model):
 
     @api.model
     def _find_matching_lines(self, values):
+        supplier = self.env['lunch.product'].browse(values.get('product_id')).supplier_id
+        date = fields.Date.today()
+        if supplier.order_deadline_passed and supplier.available_nextday:
+            date = date + relativedelta(days=1)
         domain = [
             ('user_id', '=', values.get('user_id', self.default_get(['user_id'])['user_id'])),
             ('product_id', '=', values.get('product_id', False)),
-            ('date', '=', fields.Date.today()),
+            ('date', '=', date),
             ('note', '=', values.get('note', False)),
         ]
         toppings = values.get('toppings', [])
@@ -183,7 +190,7 @@ class LunchOrder(models.Model):
             line.display_toppings = ' + '.join(toppings.mapped('name'))
 
     def update_quantity(self, increment):
-        for line in self.filtered(lambda line: line.state != 'confirmed'):
+        for line in self.filtered(lambda line: line.state not in ['sent', 'confirmed']):
             if line.quantity <= -increment:
                 # TODO: maybe unlink the order?
                 line.active = False
@@ -198,6 +205,15 @@ class LunchOrder(models.Model):
         """
         # YTI FIXME: Find a way to drop this.
         return True
+
+    @api.depends('state', 'order_deadline_passed', 'available_nextday')
+    def _compute_date(self):
+        today = fields.Date.context_today(self)
+        for order in self.filtered(lambda o: o.state == 'new'):
+            if order.order_deadline_passed and order.available_nextday:
+                order.date = today + relativedelta(days=1)
+            else:
+                order.date = today
 
     def _check_wallet(self):
         self.flush()
@@ -230,13 +246,16 @@ class LunchOrder(models.Model):
         return action
 
     def action_confirm(self):
-        self.write({'state': 'confirmed'})
+        self.filtered(lambda o: o.date <= fields.Date.context_today(self)).write({'state': 'confirmed'})
 
     def action_cancel(self):
         self.write({'state': 'cancelled'})
 
     def action_reset(self):
         self.write({'state': 'ordered'})
+
+    def action_send(self):
+        self.state = 'sent'
 
     def action_notify(self):
         self -= self.filtered('notified')
