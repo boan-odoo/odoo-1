eab4cfe50d89afb9c3dd25a8ca6a4a264afa50cc
Mantavya Gajjar <mga@openerp.com>
2021-12-03 00:03:26 +0530

    [IMP] l10n_in: deprecate multiple GSTN units in a single company.
    Deprecate the implementation of multiple GSTN units using journal id on sales, purchase and warehouse. Use multi-company incase you need the multiple GSTN.

--- addons/l10n_in_purchase/models/purchase_order.py
+++ addons/l10n_in_purchase/models/purchase_order.py
@@ -8,8 +8,9 @@ from odoo.addons.purchase.models.purchase import PurchaseOrder as Purchase
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    l10n_in_journal_id = fields.Many2one('account.journal', string="Journal", \
-        states=Purchase.READONLY_STATES, domain="[('type', '=', 'purchase')]")
+    #Deprecated field, use multi-company instead of this field
+    l10n_in_journal_id = fields.Many2one('account.journal', string="Journal (Deprecated)", \
+        states=Purchase.READONLY_STATES, domain="[('type', '=', 'purchase')]", deprecated=True)
     l10n_in_gst_treatment = fields.Selection([
             ('regular', 'Registered Business - Regular'),
             ('composition', 'Registered Business - Composition'),
@@ -20,14 +21,6 @@ class PurchaseOrder(models.Model):
             ('deemed_export', 'Deemed Export')
         ], string="GST Treatment", states=Purchase.READONLY_STATES, compute="_compute_l10n_in_gst_treatment", store=True)
     l10n_in_company_country_code = fields.Char(related='company_id.account_fiscal_country_id.code', string="Country code")
-
-    @api.onchange('company_id')
-    def l10n_in_onchange_company_id(self):
-        if self.l10n_in_company_country_code == 'IN':
-            domain = [('company_id', '=', self.company_id.id), ('type', '=', 'purchase')]
-            journal = self.env['account.journal'].search(domain, limit=1)
-            if journal:
-                self.l10n_in_journal_id = journal.id
 
     @api.depends('partner_id')
     def _compute_l10n_in_gst_treatment(self):
