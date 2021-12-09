3190595c783793cf176fc262020dd0461f2a7166
Ruben Gomes <rugo@odoo.com>
2021-12-09 16:22:39 +0100

    add tax report

--- addons/l10n_bg/__manifest__.py
+++ addons/l10n_bg/__manifest__.py
@@ -19,10 +19,11 @@
     'data': [
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
+        'data/account.group.template.csv',
         'data/l10n_bg_chart_data.xml',
+        'data/tax_report.xml',
         'data/account_tax_group_data.xml',
         'data/account_tax_template_data.xml',
-        'data/account.group.template.csv',
         'data/account_chart_template_configure_data.xml',
     ],
     'demo': [
