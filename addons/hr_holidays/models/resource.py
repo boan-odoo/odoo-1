11930372c21306f0ecd37886db37be6b10309b51
Dossogne Bertrand <bedo@odoo.com>
2021-12-23 14:02:18 +0100

    working split

--- addons/hr_holidays/models/resource.py
+++ addons/hr_holidays/models/resource.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from pytz import timezone, UTC
 from odoo import fields, models, api, _
 from datetime import timedelta
 from odoo.tools import date_utils
@@ -32,44 +33,48 @@ class CalendarLeaves(models.Model):
                 })
         return time_domain_dict
 
-    def _split_leave_on_gto(self, leave): #gto = global time off
-        self.ensure_one()
+    def _split_leave_on_gto(self, leave, gto): #gto = global time off
         leave_start = date_utils.start_of(leave['date_from'], 'day')
-        leave_end = date_utils.end_of(leave['date_to'], 'day')
-        gto_start = date_utils.start_of(self['date_from'], 'day')
-        gto_end = date_utils.end_of(self['date_to'], 'day')
+        leave_end = date_utils.end_of(leave['date_to'] - timedelta(seconds=1), 'day')
+        gto_start = date_utils.start_of(gto['date_from'], 'day')
+        gto_end = date_utils.end_of(gto['date_to'], 'day')
+        leave_tz = timezone(leave['employee_id']['resource_id']['tz'])
 
         if gto_start <= leave_start\
                 and gto_end > leave_start\
                 and gto_end < leave_end:
             leave.write({
-                'date_from': gto_end + timedelta(seconds=1)
+                'date_from': leave_tz.localize(gto_end + timedelta(seconds=1))\
+                        .astimezone(UTC).replace(tzinfo=None)
             })
             return self.env['hr.leave'].sudo()
         if gto_start > leave_start\
                 and gto_end < leave_end:
             copys = {
                 'date_from': leave['date_from'],
-                'date_to': gto_start - timedelta(seconds=1)
+                'date_to': leave_tz.localize(gto_start - timedelta(seconds=1))\
+                        .astimezone(UTC).replace(tzinfo=None)
             }
             leave.write({
-                'date_from': gto_end + timedelta(seconds=1)
+                'date_from': leave_tz.localize(gto_end + timedelta(seconds=1))\
+                        .astimezone(UTC).replace(tzinfo=None)
             })
             return leave.copy(copys)
         if gto_start > leave_start\
                 and gto_start < leave_end\
                 and gto_end >= leave_end:
             leave.write({
-                'date_to': gto_start - timedelta(seconds=1)
+                'date_to': leave_tz.localize(gto_start - timedelta(seconds=1))\
+                        .astimezone(UTC).replace(tzinfo=None)
             })
             return self.env['hr.leave'].sudo()
 
     def _split_leave(self, leave, time_domain_dict):
         new_leaves = self.env['hr.leave'].sudo()
-        for record in self\
-                .search(['|', ('date_to', '>', leave['date_from']), ('date_from', '<', leave['date_to'])])\
-                .sorted(key=lambda r: r.date_from):
-            new_leave = record._split_leave_on_gto(leave)
+        for record in sorted(
+                filter(lambda r: r['date_to'] > leave['date_from'] and r['date_from'] < leave['date_to'], time_domain_dict),
+                key=lambda r: r['date_from']):
+            new_leave = self._split_leave_on_gto(leave, record)
             if new_leave:
                 new_leaves |= new_leave
         return new_leaves
@@ -80,25 +85,24 @@ class CalendarLeaves(models.Model):
             leaves = self.env['hr.leave'].search(domain)
             if not leaves:
                 return
-            ordered_leaves = {
-                'approved': leaves.filtered(lambda l: l.state in ['validate1', 'validate']),
-                'validated': leaves.filtered(lambda l: l.state == 'validate'),
-            }
-            ordered_leaves['validated'].action_refuse()
-            ordered_leaves['approved'].action_draft()
+            approved_leaves = leaves.filtered(lambda l: l.state in ['validate1', 'validate'])
+            validated_leaves = leaves.filtered(lambda l: l.state == 'validate')
+            validated_leaves.action_refuse()
+            approved_leaves.action_draft()
             previous_durations = [leave.number_of_days for leave in leaves]
             leaves._compute_number_of_days()
             for previous_duration, leave in zip(previous_durations, leaves):
-                if leave.number_of_days > previous_duration:
+                if leave.number_of_days > previous_duration\
+                        and leave['holiday_status_id']['name'] != 'Sick Time Off':
                     new_leaves = self._split_leave(leave, time_domain_dict)
-                    if leave - ordered_leaves['approved']:
-                        ordered_leaves['approved'] |= new_leaves
-                    if leave - ordered_leaves['validated']:
-                        ordered_leaves['validated'] |= new_leaves
+                    if leave in approved_leaves:
+                        approved_leaves |= new_leaves
+                    if leave in validated_leaves:
+                        validated_leaves |= new_leaves
                     leaves |= new_leaves
-            ordered_leaves['approved'].action_confirm()
-            ordered_leaves['approved'].action_approve()
-            leaves_to_validate = ordered_leaves['validated'].filtered(lambda l: l.number_of_days > 0)
+            approved_leaves.filtered(lambda l: l.state == 'draft').action_confirm()
+            approved_leaves.action_approve()
+            leaves_to_validate = validated_leaves.filtered(lambda l: l.number_of_days > 0 and l.state != 'validate')
             leaves_to_validate.action_validate()
             for leave in leaves:
                 if leave.number_of_days == 0.0:
@@ -111,7 +115,7 @@ class CalendarLeaves(models.Model):
     def create(self, vals_list):
         res = super().create(vals_list)
         time_domain_dict = res._get_time_domain_dict()
-        if(time_domain_dict):
+        if time_domain_dict :
             self._reevaluate_leaves(time_domain_dict)
         return res
 
@@ -119,7 +123,7 @@ class CalendarLeaves(models.Model):
         time_domain_dict = self._get_time_domain_dict()
         res = super().write(vals)
         time_domain_dict = self._get_time_domain_dict(time_domain_dict)
-        if(time_domain_dict):
+        if time_domain_dict :
             self._reevaluate_leaves(time_domain_dict)
 
         return res
@@ -127,7 +131,7 @@ class CalendarLeaves(models.Model):
     def unlink(self):
         time_domain_dict = self._get_time_domain_dict()
         res = super().unlink()
-        if(time_domain_dict):
+        if time_domain_dict :
             self._reevaluate_leaves(time_domain_dict)
 
         return res
