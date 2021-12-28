bedef54bb5c28752b95f872c34896a207baffc35
Ipsita Borisagar <ibo@odoo.com>
2021-12-28 15:32:24 +0530

    [IMP] mail: activity keyboard navigation improvement
    PURPOSE
    
    Improve some mail activity issues regarding keyboard navigation.
    
    SPCIFICATION
    
    When done an activity using the record list view, allowing user ENTER key
    support on "Done" or "Done and Schedule Next" button to post feedback and
    mark the activity as done.
    
    Task-2528109

--- addons/mail/static/tests/chatter_tests.js
+++ addons/mail/static/tests/chatter_tests.js
@@ -373,6 +373,100 @@ QUnit.test('list activity exception widget with activity', async function (asser
     list.destroy();
 });
 
+QUnit.test('list activity widget: done the acitivty with "ENTER" keyboard shortcut', async function (assert) {
+    assert.expect(2);
+
+    const currentUser = this.data['res.users'].records.find(user =>
+        user.id === this.data.currentUserId
+    );
+    Object.assign(currentUser, {
+        activity_ids: [1],
+        activity_state: 'today',
+        activity_summary: 'Call with Al',
+        activity_type_id: 3,
+    });
+    this.data['mail.activity'].records.push({
+        activity_type_id: 3,
+        can_write: true,
+        create_uid: this.data.currentUserId,
+        display_name: "Call with Al",
+        date_deadline: moment().format("YYYY-MM-DD"),
+        id: 1,
+        state: "today",
+        user_id: this.data.currentUserId,
+    });
+
+    const { widget: list } = await start({
+        hasView: true,
+        View: ListView,
+        model: 'res.users',
+        data: this.data,
+        arch: `
+            <list>
+                <field name="activity_ids" widget="list_activity"/>
+            </list>`,
+        mockRPC(route, args) {
+            if (args.method === 'action_feedback') {
+                assert.step('action_feedback');
+            }
+            return this._super(route, args);
+        },
+    });
+    await testUtils.dom.click(list.$('.o_activity_btn span'));
+    await testUtils.dom.click(list.$('.o_mark_as_done:first'));
+    await testUtils.fields.triggerKeydown(list.$('.o_activity_popover_done'), 'enter');
+    assert.verifySteps(['action_feedback']);
+
+    list.destroy();
+});
+
+QUnit.test('list activity widget: done and schedule the next acitivty with "ENTER" keyboard shortcut', async function (assert) {
+    assert.expect(2);
+
+    const currentUser = this.data['res.users'].records.find(user =>
+        user.id === this.data.currentUserId
+    );
+    Object.assign(currentUser, {
+        activity_ids: [1],
+        activity_state: 'today',
+        activity_summary: 'Call with Al',
+        activity_type_id: 3,
+    });
+    this.data['mail.activity'].records.push({
+        activity_type_id: 3,
+        can_write: true,
+        create_uid: this.data.currentUserId,
+        display_name: "Call with Al",
+        date_deadline: moment().format("YYYY-MM-DD"),
+        id: 1,
+        state: "today",
+        user_id: this.data.currentUserId,
+    });
+
+    const { widget: list } = await start({
+        hasView: true,
+        View: ListView,
+        model: 'res.users',
+        data: this.data,
+        arch: `
+            <list>
+                <field name="activity_ids" widget="list_activity"/>
+            </list>`,
+        mockRPC(route, args) {
+            if (args.method === 'action_feedback_schedule_next') {
+                assert.step('action_feedback_schedule_next');
+            }
+            return this._super(route, args);
+        },
+    });
+    await testUtils.dom.click(list.$('.o_activity_btn span'));
+    await testUtils.dom.click(list.$('.o_mark_as_done:first'));
+    await testUtils.fields.triggerKeydown(list.$('.o_activity_popover_done_next'), 'enter');
+    assert.verifySteps(['action_feedback_schedule_next']);
+
+    list.destroy();
+});
+
 QUnit.module('FieldMany2ManyTagsEmail', {
     beforeEach() {
         beforeEach(this);
