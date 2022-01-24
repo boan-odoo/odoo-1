7690af9fbc531f93c26c1d548f945ebc1a4a6f50
Géry Debongnie <ged@odoo.com>
2022-01-24 11:39:58 +0100

    update owl to next-rendering

--- addons/web/static/src/search/search_model.js
+++ addons/web/static/src/search/search_model.js
@@ -17,7 +17,7 @@ import {
 } from "./utils/dates";
 import { FACET_ICONS } from "./utils/misc";
 
-const { EventBus } = owl;
+const { EventBus, toRaw } = owl;
 const { DateTime } = luxon;
 
 /**
@@ -215,7 +215,10 @@ export class SearchModel extends EventBus {
         const { comparison, context, domain, groupBy, orderBy } = config;
 
         this.globalComparison = comparison;
-        this.globalContext = context || {};
+        // OWL-NEW-RENDERING
+        // maybe we need to do the same for the other
+        // not needed for the reload ??
+        this.globalContext = toRaw(context || {});
         this.globalDomain = domain || [];
         this.globalGroupBy = groupBy || [];
         this.globalOrderBy = orderBy || [];
