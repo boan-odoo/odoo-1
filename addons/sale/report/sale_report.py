5471045ef3894f3b3bb1238889ed5f2955a22ba3
Horacio Tellez <hote@odoo.com>
2022-02-02 09:45:11 +0100

    [FIX] sale: Report on converted currency
    Before when reporting in the sale module in a multi-company
    environment currencies where not converted to a unique currency
    before mathematical operations (sum, avg, ...).
    Now before mathematical operations currencies are converted to
    the currency of the company currently selected.
    
    Task - 2696759

--- addons/sale/report/sale_report.py
+++ addons/sale/report/sale_report.py
@@ -62,42 +62,42 @@ class SaleReport(models.Model):
         if not fields:
             fields = {}
         select_ = """
-            coalesce(min(l.id), -s.id) as id,
-            l.product_id as product_id,
-            t.uom_id as product_uom,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as product_uom_qty,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_delivered / u.factor * u2.factor) ELSE 0 END as qty_delivered,
-            CASE WHEN l.product_id IS NOT NULL THEN SUM((l.product_uom_qty - l.qty_delivered) / u.factor * u2.factor) ELSE 0 END as qty_to_deliver,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_invoiced / u.factor * u2.factor) ELSE 0 END as qty_invoiced,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END as qty_to_invoice,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_total,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_subtotal / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_subtotal,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_to_invoice / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_to_invoice,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_invoiced / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_invoiced,
-            count(*) as nbr,
-            s.name as name,
-            s.date_order as date,
-            s.state as state,
-            s.partner_id as partner_id,
-            s.user_id as user_id,
-            s.company_id as company_id,
-            s.campaign_id as campaign_id,
-            s.medium_id as medium_id,
-            s.source_id as source_id,
-            extract(epoch from avg(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
-            t.categ_id as categ_id,
-            s.pricelist_id as pricelist_id,
-            s.analytic_account_id as analytic_account_id,
-            s.team_id as team_id,
+            COALESCE(min(l.id), -s.id) AS id,
+            l.product_id AS product_id,
+            t.uom_id AS product_uom,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS product_uom_qty,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_delivered / u.factor * u2.factor) ELSE 0 END AS qty_delivered,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM((l.product_uom_qty - l.qty_delivered) / u.factor * u2.factor) ELSE 0 END AS qty_to_deliver,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_invoiced / u.factor * u2.factor) ELSE 0 END AS qty_invoiced,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END AS qty_to_invoice,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_total * CASE COALESCE(currency_table.rate, 0) WHEN 0 THEN 1.0 ELSE currency_table.rate END) ELSE 0 END AS price_total,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.price_subtotal * CASE COALESCE(currency_table.rate, 0) WHEN 0 THEN 1.0 ELSE currency_table.rate END) ELSE 0 END AS price_subtotal,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_to_invoice * CASE COALESCE(currency_table.rate, 0) WHEN 0 THEN 1.0 ELSE currency_table.rate END) ELSE 0 END AS untaxed_amount_to_invoice,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(l.untaxed_amount_invoiced * CASE COALESCE(currency_table.rate, 0) WHEN 0 THEN 1.0 ELSE currency_table.rate END) ELSE 0 END AS untaxed_amount_invoiced,
+            count(*) AS nbr,
+            s.name AS name,
+            s.date_order AS date,
+            s.state AS state,
+            s.partner_id AS partner_id,
+            s.user_id AS user_id,
+            s.company_id AS company_id,
+            s.campaign_id AS campaign_id,
+            s.medium_id AS medium_id,
+            s.source_id AS source_id,
+            extract(epoch from AVG(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) AS delay,
+            t.categ_id AS categ_id,
+            s.pricelist_id AS pricelist_id,
+            s.analytic_account_id AS analytic_account_id,
+            s.team_id AS team_id,
             p.product_tmpl_id,
-            partner.country_id as country_id,
-            partner.industry_id as industry_id,
-            partner.commercial_partner_id as commercial_partner_id,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(p.weight * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as weight,
-            CASE WHEN l.product_id IS NOT NULL THEN sum(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as volume,
-            l.discount as discount,
-            CASE WHEN l.product_id IS NOT NULL THEN sum((l.price_unit * l.product_uom_qty * l.discount / 100.0 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END))ELSE 0 END as discount_amount,
-            s.id as order_id
+            partner.country_id AS country_id,
+            partner.industry_id AS industry_id,
+            partner.commercial_partner_id AS commercial_partner_id,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(p.weight * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS weight,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END AS volume,
+            l.discount AS discount,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM((l.price_unit * l.product_uom_qty * l.discount / 100.0 * CASE COALESCE(currency_table.rate, 0) WHEN 0 THEN 1.0 ELSE currency_table.rate END))ELSE 0 END AS discount_amount,
+            s.id AS order_id
         """
 
         for field in fields.values():
@@ -107,15 +107,23 @@ class SaleReport(models.Model):
     def _from_sale(self, from_clause=''):
         from_ = """
                 sale_order_line l
-                      right outer join sale_order s on (s.id=l.order_id)
-                      join res_partner partner on s.partner_id = partner.id
-                        left join product_product p on (l.product_id=p.id)
-                            left join product_template t on (p.product_tmpl_id=t.id)
-                    left join uom_uom u on (u.id=l.product_uom)
-                    left join uom_uom u2 on (u2.id=t.uom_id)
-                    left join product_pricelist pp on (s.pricelist_id = pp.id)
-                %s
-        """ % from_clause
+                      RIGHT OUTER JOIN sale_order s ON (s.id=l.order_id)
+                      JOIN res_partner partner ON s.partner_id = partner.id
+                        LEFT JOIN product_product p ON (l.product_id=p.id)
+                            LEFT JOIN product_template t ON (p.product_tmpl_id=t.id)
+                    LEFT JOIN uom_uom u ON (u.id=l.product_uom)
+                    LEFT JOIN uom_uom u2 ON (u2.id=t.uom_id)
+                    LEFT JOIN product_pricelist pp ON (s.pricelist_id = pp.id)
+                    JOIN {currency_table} ON currency_table.company_id = s.company_id
+                    {from_clause} 
+        """.format(
+            from_clause=from_clause,
+            currency_table=self.env['res.currency']._get_query_currency_table(
+                {
+                    'multi_company': True,
+                    'date': {'date_to': fields.Date.today()}
+                }),
+            )
         return from_
 
     def _group_by_sale(self, groupby=''):
@@ -152,7 +160,6 @@ class SaleReport(models.Model):
         return '%s (SELECT %s FROM %s WHERE l.display_type IS NULL GROUP BY %s)' % \
                (with_, self._select_sale(fields), self._from_sale(from_clause), self._group_by_sale(groupby))
 
-    def init(self):
-        # self._table = sale_report
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (self._table, self._query()))
+    @property
+    def _table_query(self):
+        return self._query()
