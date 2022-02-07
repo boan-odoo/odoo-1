danielosser/odoo

[IMP] website_slides: allow to mark content as "(un)done"
Purpose
=======
Allow the users to themselves mark their course as "done" or "not done"

Specifications
==============
When the course is a "Training" in fullscreen or in non-fullscreen, or
when the course is a "Documentation" in fullscreen mode, allow the user
to click on the "done" button in the sidebar.

When the course is a "Documentation" in non-fullscreen mode, add a
button "Mark Not Done" when the slide is done.

The quiz and the certification cannot be marked as done manually, the
user has to complete the questions for that.

A certification cannot be marked as "Not Done" (but a quiz can).

In the non-fullscreen view, mark content as done automatically like we
do for the fullscreen view.

Technical
=========
As the fullscreen view and the non-fullscreen view will share a lot of
code, make a common class to avoid redundancy.

Task-2604792

addons/website_slides/models/slide_slide.py
@@ -45,9 +45,15 @@ class SlidePartnerRelation(models.Model):
         return res
 
     def write(self, values):
+        slides_completed_change = None
+        if 'completed' in values:
+            slides_completed_change = self.filtered(lambda spr: spr.completed != values['completed'])
+
         res = super(SlidePartnerRelation, self).write(values)
-        if values.get('completed'):
-            self._set_completed_callback()
+
+        if slides_completed_change:
+            slides_completed_change._set_completed_callback()
+
         return res
 
     def _set_completed_callback(self):
@@ -123,6 +129,8 @@ class Slide(models.Model):
     quiz_third_attempt_reward = fields.Integer("Reward: third attempt", default=5,)
     quiz_fourth_attempt_reward = fields.Integer("Reward: every attempt after the third try", default=2)
     # content
+    can_be_undone = fields.Boolean('Can Be Undone', compute='_compute_can_be_undone')
+    can_be_skipped = fields.Boolean('Can Be Skipped', compute='_compute_can_be_undone')
     slide_category = fields.Selection([
         ('infographic', 'Image'),
         ('article', 'Article'),
@@ -248,6 +256,18 @@ class Slide(models.Model):
                     current_category = slide
                 elif slide.category_id != current_category:
                     slide.category_id = current_category.id
+
+    @api.depends('slide_type', 'question_ids', 'channel_id.is_member')
+    @api.depends_context('uid')
+    def _compute_can_be_undone(self):
+        """Determine if the slide can be marked as (un)completed."""
+        for slide in self:
+            slide.can_be_undone = slide.channel_id.is_member
+            slide.can_be_skipped = (
+                slide.channel_id.is_member
+                and slide.slide_type != 'quiz'
+                and not slide.question_ids
+            )
 
     @api.depends('question_ids')
     def _compute_questions_count(self):
@@ -843,16 +863,48 @@ class Slide(models.Model):
             'vote': 0,
             'completed': True} for new_slide in new_slides])
 
+        self.mapped('channel_id').invalidate_cache(['completed', 'completion'])
+
         return True
 
-    def _action_set_quiz_done(self):
+    def action_set_uncompleted(self):
         if any(not slide.channel_id.is_member for slide in self):
-            raise UserError(_('You cannot mark a slide quiz as completed if you are not among its members.'))
+            raise UserError(_('You cannot mark a slide as uncompleted if you are not among its members.'))
+
+        if any(not slide.can_be_undone for slide in self):
+            raise UserError(_('Those slides can not be undone.'))
+
+        target_partner = self.env.user.partner_id
+
+        # Remove the Karma point gained
+        self._action_set_quiz_done(completed=False)
+
+        self.env['slide.slide.partner'].sudo().search([
+            ('slide_id', 'in', self.ids),
+            ('partner_id', '=', target_partner.id)
+        ]).completed = False
+
+        self.mapped('channel_id').invalidate_cache(['completed', 'completion'])
+
+        return True
+
+    def _action_set_quiz_done(self, completed=True):
+        """Add or remove karma point related to the quiz.
+
+        :param completed: True if the quiz will be marked as completed (karma will be increased)
+        """
+        if any(not slide.channel_id.is_member for slide in self):
+            raise UserError(
+                _('You cannot mark a slide quiz as completed if you are not among its members.') if completed
+                else _('You cannot mark a slide quiz as not completed if you are not among its members.')
+            )
 
         points = 0
         for slide in self:
             user_membership_sudo = slide.user_membership_id.sudo()
-            if not user_membership_sudo or user_membership_sudo.completed or not user_membership_sudo.quiz_attempts_count:
+            if not user_membership_sudo \
+               or user_membership_sudo.completed == completed \
+               or not user_membership_sudo.quiz_attempts_count:
                 continue
 
             gains = [slide.quiz_first_attempt_reward,
@@ -860,6 +912,9 @@ class Slide(models.Model):
                      slide.quiz_third_attempt_reward,
                      slide.quiz_fourth_attempt_reward]
             points += gains[user_membership_sudo.quiz_attempts_count - 1] if user_membership_sudo.quiz_attempts_count <= len(gains) else gains[-1]
+
+        if not completed:
+            points *= -1
 
         return self.env.user.sudo().add_karma(points)
 

addons/website_slides/static/src/js/slides_course_page.js
@@ -1,0 +1,147 @@
+/** @odoo-module **/
+
+import publicWidget from 'web.public.widget';
+import session from 'web.session';
+import { qweb as QWeb } from 'web.core';
+
+
+/**
+ * Global widget for both fullscreen view and non-fullscreen view of a slide course.
+ * Contains general methods to update the UI elements (progress bar, sidebar...) as well
+ * as method to mark the slide as completed / uncompleted.
+ */
+export const SlideCoursePage = publicWidget.Widget.extend({
+    events: {
+        'click button.o_wslides_button_complete': '_onClickComplete',
+    },
+
+    custom_events: {
+        'slide_completed': '_onSlideCompleted',
+        'slide_to_complete': '_onSlideToComplete',
+    },
+
+    xmlDependencies: ['/website_slides/static/src/xml/website_slides_sidebar.xml'],
+
+    /**
+     * Greens up the bullet when the slide is completed
+     *
+     * @public
+     * @param {Integer} slideId
+     * @param {Boolean} completed
+     */
+    setSlideCompleted: function (slide, completed = true) {
+        const $button = this.$(`.o_wslides_sidebar_done_button[data-id=${slide.id}]`);
+
+        const newButton = QWeb.render('website.slides.sidebar.done.button', {
+            slideId: slide.id,
+            slideCompleted: completed,
+            canBeUndone: slide.canBeUndone,
+            canBeSkipped: slide.canBeSkipped,
+        });
+
+        if ($button.length) {
+            $button.replaceWith(newButton);
+            const $elem = this.$(`.o_wslides_sidebar_done_button[data-id="${slide.id}"]`);
+            $elem.data('completed', completed);
+        }
+    },
+
+    /**
+     * Updates the progressbar whenever a lesson is completed
+     *
+     * @public
+     * @param {*} channelCompletion
+     */
+    updateProgressbar: function (channelCompletion) {
+        const completion = Math.min(100, channelCompletion);
+
+        const $completed = $('.o_wslides_channel_completion_completed');
+        const $progressbar = $('.o_wslides_channel_completion_progressbar');
+
+        if (completion < 100) {
+            // Hide the "Completed" text and show the progress bar
+            $completed.addClass('d-none');
+            $progressbar.removeClass('d-none').addClass('d-flex');
+        } else {
+            // Hide the progress bar and show the "Completed" text
+            $completed.removeClass('d-none');
+            $progressbar.addClass('d-none').removeClass('d-flex');
+        }
+
+        $progressbar.find('.progress-bar').css('width', `${completion}%`);
+        $progressbar.find('.o_wslides_progress_percentage').text(completion);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Once the completion conditions are filled,
+     * rpc call to set the relation between the slide and the user as "completed"
+     *
+     * @private
+     * @param {Integer} slideId: the id of slide to set as completed
+     * @param {Boolean} completed: true to mark the slide as completed
+     *     false to mark the slide as not completed
+     */
+    _setCompleted: async function (slide, completed = true) {
+        if (!!slide.completed === !!completed) {
+            // no useless RPC call
+            return;
+        }
+
+        const data = await this._rpc({
+            route: `/slides/slide/${completed ? 'set_completed' : 'set_uncompleted'}`,
+            params: {slide_id: slide.id},
+        });
+
+        this.setSlideCompleted(slide, completed);
+        this.updateProgressbar(data.channel_completion);
+
+        slide.completed = true;
+    },
+
+    _getSlide: function (slideId) {
+        return $(`.o_wslides_sidebar_done_button[data-id="${slideId}"]`).data();
+    },
+
+    //--------------------------------------------------------------------------
+    // Handler
+    //--------------------------------------------------------------------------
+    _onClickComplete: function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        const $button = $(ev.currentTarget).parents('.o_wslides_sidebar_done_button');
+
+        const slideData = $button.data();
+        const isCompleted = Boolean(slideData.completed);
+
+        this._setCompleted(slideData, !isCompleted);
+    },
+
+    /**
+     * The slide has been completed, update the UI
+     */
+    _onSlideCompleted: function (ev) {
+        const slideId = ev.data.slideId;
+        const completed = ev.data.completed;
+        const slide = this._getSlide(slideId);
+        if (slide) {
+            // Just joined the course (e.g. When "Submit & Join" action), update the UI
+            this.setSlideCompleted(slide, completed);
+        }
+        this.updateProgressbar(ev.data.channel_completion);
+    },
+
+    /**
+     * Make a RPC call to complete the slide then update the UI
+     */
+    _onSlideToComplete: function (ev) {
+        if (!session.is_website_user) { // no useless RPC call
+            const slide = this._getSlide(ev.data.id);
+            this._setCompleted(slide, true);
+        }
+    }
+});

boan-odoo/odoo-1

addons/website_slides/models/slide_slide.py
@@ -45,9 +45,15 @@ class SlidePartnerRelation(models.Model):
         return res
 
     def write(self, values):
+        slides_completed_change = None
+        if 'completed' in values:
+            slides_completed_change = self.filtered(lambda spr: spr.completed != values['completed'])
+
         res = super(SlidePartnerRelation, self).write(values)
-        if values.get('completed'):
-            self._set_completed_callback()
+
+        if slides_completed_change:
+            slides_completed_change._set_completed_callback()
+
         return res
 
     def _set_completed_callback(self):
@@ -123,6 +129,8 @@ class Slide(models.Model):
     quiz_third_attempt_reward = fields.Integer("Reward: third attempt", default=5,)
     quiz_fourth_attempt_reward = fields.Integer("Reward: every attempt after the third try", default=2)
     # content
+    can_be_undone = fields.Boolean('Can Be Undone', compute='_compute_member_actions')
+    can_be_skipped = fields.Boolean('Can Be Skipped', compute='_compute_member_actions')
     slide_category = fields.Selection([
         ('infographic', 'Image'),
         ('article', 'Article'),
@@ -248,6 +256,18 @@ class Slide(models.Model):
                     current_category = slide
                 elif slide.category_id != current_category:
                     slide.category_id = current_category.id
+
+    @api.depends('slide_type', 'question_ids', 'channel_id.is_member')
+    @api.depends_context('uid')
+    def _compute_member_actions(self):
+        """Determine if the slide can be marked as (un)completed."""
+        for slide in self:
+            slide.can_be_undone = slide.channel_id.is_member
+            slide.can_be_skipped = (
+                slide.channel_id.is_member
+                and slide.slide_type != 'quiz'
+                and not slide.question_ids
+            )
 
     @api.depends('question_ids')
     def _compute_questions_count(self):
@@ -846,16 +866,48 @@ class Slide(models.Model):
             'vote': 0,
             'completed': True} for new_slide in new_slides])
 
+        self.mapped('channel_id').invalidate_cache(['completed', 'completion'])
+
         return True
 
-    def _action_set_quiz_done(self):
+    def action_set_uncompleted(self):
         if any(not slide.channel_id.is_member for slide in self):
-            raise UserError(_('You cannot mark a slide quiz as completed if you are not among its members.'))
+            raise UserError(_('You cannot mark a slide as uncompleted if you are not among its members.'))
+
+        if any(not slide.can_be_undone for slide in self):
+            raise UserError(_('Those slides can not be undone.'))
+
+        target_partner = self.env.user.partner_id
+
+        # Remove the Karma point gained
+        self._action_set_quiz_done(completed=False)
+
+        self.env['slide.slide.partner'].sudo().search([
+            ('slide_id', 'in', self.ids),
+            ('partner_id', '=', target_partner.id)
+        ]).completed = False
+
+        self.mapped('channel_id').invalidate_cache(['completed', 'completion'])
+
+        return True
+
+    def _action_set_quiz_done(self, completed=True):
+        """Add or remove karma point related to the quiz.
+
+        :param completed: True if the quiz will be marked as completed (karma will be increased)
+        """
+        if any(not slide.channel_id.is_member for slide in self):
+            raise UserError(
+                _('You cannot mark a slide quiz as completed if you are not among its members.') if completed
+                else _('You cannot mark a slide quiz as not completed if you are not among its members.')
+            )
 
         points = 0
         for slide in self:
             user_membership_sudo = slide.user_membership_id.sudo()
-            if not user_membership_sudo or user_membership_sudo.completed or not user_membership_sudo.quiz_attempts_count:
+            if not user_membership_sudo \
+               or user_membership_sudo.completed == completed \
+               or not user_membership_sudo.quiz_attempts_count:
                 continue
 
             gains = [slide.quiz_first_attempt_reward,
@@ -863,6 +915,9 @@ class Slide(models.Model):
                      slide.quiz_third_attempt_reward,
                      slide.quiz_fourth_attempt_reward]
             points += gains[user_membership_sudo.quiz_attempts_count - 1] if user_membership_sudo.quiz_attempts_count <= len(gains) else gains[-1]
+
+        if not completed:
+            points *= -1
 
         return self.env.user.sudo().add_karma(points)
 

addons/website_slides/static/src/js/slides_course_page.js
@@ -1,0 +1,147 @@
+/** @odoo-module **/
+
+import publicWidget from 'web.public.widget';
+import session from 'web.session';
+import { qweb as QWeb } from 'web.core';
+
+
+/**
+ * Global widget for both fullscreen view and non-fullscreen view of a slide course.
+ * Contains general methods to update the UI elements (progress bar, sidebar...) as well
+ * as method to mark the slide as completed / uncompleted.
+ */
+export const SlideCoursePage = publicWidget.Widget.extend({
+    events: {
+        'click button.o_wslides_button_complete': '_onClickComplete',
+    },
+
+    custom_events: {
+        'slide_completed': '_onSlideCompleted',
+        'slide_to_complete': '_onSlideToComplete',
+    },
+
+    xmlDependencies: ['/website_slides/static/src/xml/website_slides_sidebar.xml'],
+
+    /**
+     * Greens up the bullet when the slide is completed
+     *
+     * @public
+     * @param {Integer} slideId
+     * @param {Boolean} completed
+     */
+    setSlideCompleted: function (slide, completed = true) {
+        const $button = this.$(`.o_wslides_sidebar_done_button[data-id=${slide.id}]`);
+
+        const newButton = QWeb.render('website.slides.sidebar.done.button', {
+            slideId: slide.id,
+            slideCompleted: completed,
+            canBeUndone: slide.canBeUndone,
+            canBeSkipped: slide.canBeSkipped,
+        });
+
+        if ($button.length) {
+            $button.replaceWith(newButton);
+            const $elem = this.$(`.o_wslides_sidebar_done_button[data-id="${slide.id}"]`);
+            $elem.data('completed', completed);
+        }
+    },
+
+    /**
+     * Updates the progressbar whenever a lesson is completed
+     *
+     * @public
+     * @param {*} channelCompletion
+     */
+    updateProgressbar: function (channelCompletion) {
+        const completion = Math.min(100, channelCompletion);
+
+        const $completed = $('.o_wslides_channel_completion_completed');
+        const $progressbar = $('.o_wslides_channel_completion_progressbar');
+
+        if (completion < 100) {
+            // Hide the "Completed" text and show the progress bar
+            $completed.addClass('d-none');
+            $progressbar.removeClass('d-none').addClass('d-flex');
+        } else {
+            // Hide the progress bar and show the "Completed" text
+            $completed.removeClass('d-none');
+            $progressbar.addClass('d-none').removeClass('d-flex');
+        }
+
+        $progressbar.find('.progress-bar').css('width', `${completion}%`);
+        $progressbar.find('.o_wslides_progress_percentage').text(completion);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Once the completion conditions are filled,
+     * rpc call to set the relation between the slide and the user as "completed"
+     *
+     * @private
+     * @param {Integer} slideId: the id of slide to set as completed
+     * @param {Boolean} completed: true to mark the slide as completed
+     *     false to mark the slide as not completed
+     */
+    _setCompleted: async function (slide, completed = true) {
+        if (!!slide.completed === !!completed) {
+            // no useless RPC call
+            return;
+        }
+
+        const data = await this._rpc({
+            route: `/slides/slide/${completed ? 'set_completed' : 'set_uncompleted'}`,
+            params: {slide_id: slide.id},
+        });
+
+        this.setSlideCompleted(slide, completed);
+        this.updateProgressbar(data.channel_completion);
+
+        slide.completed = true;
+    },
+
+    _getSlide: function (slideId) {
+        return $(`.o_wslides_sidebar_done_button[data-id="${slideId}"]`).data();
+    },
+
+    //--------------------------------------------------------------------------
+    // Handler
+    //--------------------------------------------------------------------------
+    _onClickComplete: function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        const $button = $(ev.currentTarget).parents('.o_wslides_sidebar_done_button');
+
+        const slideData = $button.data();
+        const isCompleted = Boolean(slideData.completed);
+
+        this._setCompleted(slideData, !isCompleted);
+    },
+
+    /**
+     * The slide has been completed, update the UI
+     */
+    _onSlideCompleted: function (ev) {
+        const slideId = ev.data.slideId;
+        const completed = ev.data.completed;
+        const slide = this._getSlide(slideId);
+        if (slide) {
+            // Just joined the course (e.g. When "Submit & Join" action), update the UI
+            this.setSlideCompleted(slide, completed);
+        }
+        this.updateProgressbar(ev.data.channelCompletion);
+    },
+
+    /**
+     * Make a RPC call to complete the slide then update the UI
+     */
+    _onSlideToComplete: function (ev) {
+        if (!session.is_website_user) { // no useless RPC call
+            const slide = this._getSlide(ev.data.id);
+            this._setCompleted(slide, true);
+        }
+    }
+});