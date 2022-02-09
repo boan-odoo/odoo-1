odoo/odoo

[REF] website, website_*: adapt code to owl 2
Co-authored-by: Aaron Bohy <aab@odoo.com>
Co-authored-by: Bruno Boi <boi@odoo.com>
Co-authored-by: Géry Debongnie <ged@odoo.com>
Co-authored-by: Samuel Degueldre <sad@odoo.com>
Co-authored-by: Mathieu Duckerts-Antoine <dam@odoo.com>
Co-authored-by: Simon Genin (ges) <ges@odoo.com>
Co-authored-by: Francois (fge) <fge@odoo.com>
Co-authored-by: Michael Mattiello (mcm) <mcm@odoo.com>
Co-authored-by: Julien Mougenot <jum@odoo.com>
Co-authored-by: luvi <luvi@odoo.com>
Co-authored-by: Lucas Perais (lpe) <lpe@odoo.com>
Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>

addons/website/static/src/js/menu/debug_menu.js
@@ -11,10 +11,10 @@ const debugMenuService = {
             const systray = document.querySelector('.o_menu_systray');
             if (systray) {
                 Object.assign(env, createDebugContext(env, {categories: ["default"]}));
-                mount(DebugMenuBasic, {
-                    target: systray,
+                mount(DebugMenuBasic, systray, {
                     position: 'first-child',
                     env,
+                    templates: window.__ODOO_TEMPLATES__,
                 });
             }
         }

boan-odoo/odoo-1

addons/website/static/src/js/menu/debug_menu.js
@@ -11,10 +11,10 @@ const debugMenuService = {
             const systray = document.querySelector('.o_menu_systray');
             if (systray) {
                 Object.assign(env, createDebugContext(env, {categories: ["default"]}));
-                mount(DebugMenuBasic, {
-                    target: systray,
+                mount(DebugMenuBasic, systray, {
                     position: 'first-child',
                     env,
+                    templates: window.__OWL_TEMPLATES__,
                 });
             }
         }