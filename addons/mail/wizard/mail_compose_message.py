danielosser/odoo

[REF] mail: composer: move attachments field from onchange to computed fields
PURPOSE

Clean code by removing onchange and using editable stored computed fields.

SPECIFICATIONS

Improve composer to correctly compute fields ``attachment_ids`` in compute
methods instead of in a big onchange method and/or in a default_get.

Computation is UPDATE ME

Side effects: ~UPDATE ME

Task-2088884 (MailComposer: onchange to computed stored field

addons/mail/wizard/mail_compose_message.py
@@ -76,7 +76,8 @@ class MailComposer(models.TransientModel):
     template_id = fields.Many2one('mail.template', 'Use template', domain="[('model', '=', model)]")
     attachment_ids = fields.Many2many(
         'ir.attachment', 'mail_compose_message_ir_attachments_rel',
-        'wizard_id', 'attachment_id', 'Attachments')
+        'wizard_id', 'attachment_id', string='Attachments',
+        compute='_compute_attachment_ids', readonly=False, store=True)
     layout = fields.Char('Layout', copy=False)  # xml id of layout
     add_sign = fields.Boolean(default=True)
     # origin
@@ -162,6 +163,41 @@ class MailComposer(models.TransientModel):
                 composer._set_value_from_template('body_html', 'body')
             elif not composer.template_id or not composer.body:
                 composer.body = False
+
+    @api.depends('composition_mode', 'model', 'res_id', 'template_id')
+    def _compute_attachment_ids(self):
+        """ Attachments computation.
+
+        With template
+          * mass mail: take IDs from template
+        """
+        for composer in self:
+            if composer.template_id and composer.composition_mode == 'mass_mail' and composer.template_id.attachment_ids:
+                composer.attachment_ids = composer.template_id.attachment_ids
+            elif composer.template_id and composer.composition_mode == 'comment':
+                rendered_values = composer.template_id._generate_attachments(
+                    [composer.res_id],
+                    ['attachments', 'attachment_ids']
+                )[composer.res_id]
+                # transform attachments into attachment_ids; not attached to the
+                # document because this will be done further in the posting
+                # process, allowing to clean database if email not send
+                attachment_ids = []
+                if rendered_values.get('attachments'):
+                    attachment_ids = self.env['ir.attachment'].create([
+                        {'name': attach_fname,
+                         'datas': attach_datas,
+                         'res_model': 'mail.compose.message',
+                         'res_id': 0,
+                         'type': 'binary',    # override default_type from context, possibly meant for another model!
+                        } for attach_fname, attach_datas in rendered_values.pop('attachments')
+                    ]).ids
+                if rendered_values.get('attachment_ids'):
+                    attachment_ids += rendered_values['attachment_ids']
+                if attachment_ids:
+                    composer.attachment_ids = attachment_ids
+            else:
+                composer.attachment_ids = False
 
     @api.depends('template_id', 'author_id', 'composition_mode', 'model', 'res_id')
     def _compute_email_from(self):
@@ -598,37 +634,19 @@ class MailComposer(models.TransientModel):
         """
         values = {}
         if template_id and composition_mode == 'mass_mail':
-            template = self.env['mail.template'].browse(template_id)
-            if template.attachment_ids:
-                values['attachment_ids'] = [att.id for att in template.attachment_ids]
+            pass
         elif template_id:
             values = self.generate_email_for_composer(
                 template_id, [res_id],
-                ['email_to', 'partner_to', 'email_cc',
-                 'attachments', 'attachment_ids']
+                ['email_to', 'partner_to', 'email_cc']
             )[res_id]
-            # transform attachments into attachment_ids; not attached to the document because this will
-            # be done further in the posting process, allowing to clean database if email not send
-            attachment_ids = []
-            Attachment = self.env['ir.attachment']
-            for attach_fname, attach_datas in values.pop('attachments', []):
-                data_attach = {
-                    'name': attach_fname,
-                    'datas': attach_datas,
-                    'res_model': 'mail.compose.message',
-                    'res_id': 0,
-                    'type': 'binary',  # override default_type from context, possibly meant for another model!
-                }
-                attachment_ids.append(Attachment.create(data_attach).id)
-            if values.get('attachment_ids', []) or attachment_ids:
-                values['attachment_ids'] = [Command.set(values.get('attachment_ids', []) + attachment_ids)]
         else:
             default_values = self.with_context(
                 default_composition_mode=composition_mode,
                 default_model=model,
                 default_res_id=res_id
-            ).default_get(['composition_mode', 'parent_id', 'partner_ids', 'attachment_ids'])
-            values = dict((key, default_values[key]) for key in ['partner_ids', 'attachment_ids'] if key in default_values)
+            ).default_get(['composition_mode', 'parent_id', 'partner_ids'])
+            values = dict((key, default_values[key]) for key in ['partner_ids'] if key in default_values)
 
         if values.get('body_html'):
             values['body'] = values.pop('body_html')

boan-odoo/odoo-1

addons/mail/wizard/mail_compose_message.py
@@ -81,7 +81,8 @@ class MailComposer(models.TransientModel):
     template_id = fields.Many2one('mail.template', 'Use template', domain="[('model', '=', model)]")
     attachment_ids = fields.Many2many(
         'ir.attachment', 'mail_compose_message_ir_attachments_rel',
-        'wizard_id', 'attachment_id', 'Attachments')
+        'wizard_id', 'attachment_id', string='Attachments',
+        compute='_compute_attachment_ids', readonly=False, store=True)
     email_layout_xmlid = fields.Char('Email Notification Layout', copy=False)
     email_add_signature = fields.Boolean(default=True)
     # origin
@@ -170,6 +171,41 @@ class MailComposer(models.TransientModel):
                 composer._set_value_from_template('body_html', 'body')
             elif not composer.template_id or not composer.body:
                 composer.body = False
+
+    @api.depends('composition_mode', 'model', 'res_id', 'template_id')
+    def _compute_attachment_ids(self):
+        """ Attachments computation.
+
+        With template
+          * mass mail: take IDs from template
+        """
+        for composer in self:
+            if composer.template_id and composer.composition_mode == 'mass_mail' and composer.template_id.attachment_ids:
+                composer.attachment_ids = composer.template_id.attachment_ids
+            elif composer.template_id and composer.composition_mode == 'comment':
+                rendered_values = composer.template_id._generate_template_attachments(
+                    [composer.res_id],
+                    ['attachments', 'attachment_ids']
+                )[composer.res_id]
+                # transform attachments into attachment_ids; not attached to the
+                # document because this will be done further in the posting
+                # process, allowing to clean database if email not send
+                attachment_ids = []
+                if rendered_values.get('attachments'):
+                    attachment_ids = self.env['ir.attachment'].create([
+                        {'name': attach_fname,
+                         'datas': attach_datas,
+                         'res_model': 'mail.compose.message',
+                         'res_id': 0,
+                         'type': 'binary',    # override default_type from context, possibly meant for another model!
+                        } for attach_fname, attach_datas in rendered_values.pop('attachments')
+                    ]).ids
+                if rendered_values.get('attachment_ids'):
+                    attachment_ids += rendered_values['attachment_ids']
+                if attachment_ids:
+                    composer.attachment_ids = attachment_ids
+            else:
+                composer.attachment_ids = False
 
     @api.depends('template_id', 'author_id', 'composition_mode', 'model', 'res_id')
     def _compute_email_from(self):
@@ -613,39 +649,21 @@ class MailComposer(models.TransientModel):
         """
         values = {}
         if template_id and composition_mode == 'mass_mail':
-            template = self.env['mail.template'].browse(template_id)
-            if template.attachment_ids:
-                values['attachment_ids'] = [att.id for att in template.attachment_ids]
+            pass
         elif template_id:
             values = self._generate_email_for_composer(
                 template_id, [res_id],
-                ['email_to', 'partner_to', 'email_cc',
-                 'attachments', 'attachment_ids']
+                ['email_to', 'partner_to', 'email_cc']
             )[res_id]
-            # transform attachments into attachment_ids; not attached to the document because this will
-            # be done further in the posting process, allowing to clean database if email not send
-            attachment_ids = []
-            Attachment = self.env['ir.attachment']
-            for attach_fname, attach_datas in values.pop('attachments', []):
-                data_attach = {
-                    'name': attach_fname,
-                    'datas': attach_datas,
-                    'res_model': 'mail.compose.message',
-                    'res_id': 0,
-                    'type': 'binary',  # override default_type from context, possibly meant for another model!
-                }
-                attachment_ids.append(Attachment.create(data_attach).id)
-            if values.get('attachment_ids', []) or attachment_ids:
-                values['attachment_ids'] = [Command.set(values.get('attachment_ids', []) + attachment_ids)]
         else:
             default_values = self.with_context(
                 default_composition_mode=composition_mode,
                 default_model=model,
                 default_res_id=res_id
-            ).default_get(['composition_mode', 'parent_id', 'partner_ids', 'attachment_ids'])
+            ).default_get(['composition_mode', 'parent_id', 'partner_ids'])
             values = dict(
                 (key, default_values[key])
-                for key in ['partner_ids', 'attachment_ids'] if key in default_values)
+                for key in ['partner_ids'] if key in default_values)
 
         if values.get('body_html'):
             values['body'] = values.pop('body_html')