bd8f97d0067e7f4bf7d0e94105ea8b31449d0922
momegahed <mome@odoo.com>
2022-02-21 18:01:08 +0100

    [FIX] website_sale: add product snippet filter on all websites
    If applied, this commit will fix the following bug by changing
    the implementation of adding the product filter to websites
    to include the rest of the websites not only the default one
    
    Steps to reproduce:
    
    1- install website module
    2- create more than 1 website
    3- install website_sale module
    4- the dynamic product snippet only works on the default website
    
    Bug:
    
    the ```website.snippet.filter``` for products is added to the default
    website only
    
    Fix:
    
    convert to post_init_hook and include all websites
    
    DON'T forward port to v15 onwards, the website_id field is not required
     anymore for website.snippet.filter which makes them global by default
    
    OPW-2754340

--- addons/website_sale/__init__.py
+++ addons/website_sale/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, SUPERUSER_ID
+from odoo import api, SUPERUSER_ID, _
 from . import controllers
 from . import models
 from . import wizard
@@ -17,3 +17,19 @@ def uninstall_hook(cr, registry):
     multi_company_rules = pl_rule or env['ir.rule']
     multi_company_rules += pl_item_rule or env['ir.rule']
     multi_company_rules.write({'active': True})
+
+
+def post_init_hook(cr, registry):
+    ''' Need to add product filters to previously created website
+    '''
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    action = env.ref('website_sale.dynamic_snippet_products_action', raise_if_not_found=False)
+    if action:
+        for website in env['website'].search([]):
+            env['website.snippet.filter'].create({
+                'action_server_id': action.id,
+                'field_names': 'display_name,description_sale,image_512,list_price',
+                'limit': 16,
+                'name': _('Products'),
+                'website_id': website.id,
+            })
