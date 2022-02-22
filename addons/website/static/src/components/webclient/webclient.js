8c21f3486ce46a9007562fc406f66b2de83014f0
Arthur Detroux (ard) <ard@odoo.com>
2022-02-22 15:23:16 +0100

    go in fullscreen when edit starts

--- addons/website/static/src/components/webclient/webclient.js
+++ addons/website/static/src/components/webclient/webclient.js
@@ -5,22 +5,27 @@ import { FullscreenIndication } from '@website/components/fullscreen_indication/
 import { patch } from 'web.utils';
 import { useService } from '@web/core/utils/hooks';
 
-const { useState, useExternalListener } = owl;
+const { useState, useEffect, useExternalListener } = owl;
 
 patch(WebClient.prototype, 'website_web_client', {
     setup() {
         this._super();
         this.website = useService('website');
+        this.websiteContext = useState(this.website.context);
 
         this.fullscreenState = useState({
             isFullscreen: false,
         });
 
+        useEffect(() => {
+            this.fullscreenState.isFullscreen = !!this.websiteContext.edition;
+            this.el.closest('body').classList.toggle('o_website_fullscreen', this.fullscreenState.isFullscreen);
+        }, () => [this.websiteContext.edition]);
+
         useExternalListener(document, 'keydown', (ev) => {
             // Toggle fullscreen mode when pressing escape.
             if (ev.keyCode === 27 && this.website.currentWebsite) {
                 this.fullscreenState.isFullscreen = !this.fullscreenState.isFullscreen;
-                this.el.closest('body').classList.toggle('o_website_fullscreen', this.fullscreenState.isFullscreen);
             }
         });
     },
