49f73ee5879cff786b736199af9876be4f0193fc
Laurent Stukkens (LTU) <ltu@odoo.com>
2022-02-23 11:34:26 +0100

    fixup! [IMP] project : Add project Tree view, improve some wordings and hide uom conversion when default and selected are the same

--- addons/sale_timesheet/models/product.py
+++ addons/sale_timesheet/models/product.py
@@ -35,10 +35,9 @@ class ProductTemplate(models.Model):
     def _compute_service_upsell_threshold_ratio(self):
         product_uom_hour = self.env.ref('uom.product_uom_hour')
         for record in self:
-            if not record.uom_id:
+            if not record.uom_id or product_uom_hour.factor == record.uom_id.factor:
                 record.service_upsell_threshold_ratio = False
                 continue
-            record.service_upsell_threshold_ratio = ""
             if product_uom_hour.factor != record.uom_id.factor:
                 record.service_upsell_threshold_ratio = f"(1 {record.uom_id.name} = {product_uom_hour.factor / record.uom_id.factor:.2f} Hours)"
 
